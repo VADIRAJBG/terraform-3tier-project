126cd33bbc82420eb95206b6e8794a5e58d56353
VADIRAJBG <60073920+VADIRAJBG@users.noreply.github.com>
2026-01-03 18:13:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -357,7 +357,7 @@ output "lb_dns_name" {
 
 
 resource "aws_s3_bucket" "example" {
-  bucket = "reyaz-devopsproject-bkt-123-${random_id.suffix.hex}"
+  bucket = "reyaz-devopsproject-bkt-123"
 
   tags = {
     Name        = "reyaz-devopsproject-bkt-123"
